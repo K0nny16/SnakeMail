0cafbefbe8cd788c707d8daa39c3409a20500f89
Karl Kowal <kowal.karl99@gmail.com>
2024-11-21 19:21:31 +0100

    fixade fel i html

--- extension/web/popup.js
+++ extension/web/popup.js
@@ -1,18 +1,21 @@
 document.querySelector("#analyze-btn").addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs.length === 0) {
-            console.error("No active tab found.");
+        if (tabs.length === 0 || !tabs[0].id) {
+            console.error("No active tab found or tab ID missing.");
+            return;
         }
 
         const message = { action: "analyze" };
         chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
-            if (response) {
-                console.log("Response from content script:", response);
-                // Hantera svaret här, t.ex. visa det i popupen
-                //alert("Email Content: " + response.text); 
+            if (chrome.runtime.lastError) {
+                console.error("Error sending message:", chrome.runtime.lastError.message);
+            } else if (response) {
+               //Gör något med responset.
+               console.log("Text response: ",response) 
             } else {
                 console.log("No response from content script!");
             }
         });
     });
 });
+
